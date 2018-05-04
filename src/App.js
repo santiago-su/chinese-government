2f9e13fd38526d335eea685c2c761649b23c6951
Santiago Suárez <santiago.su.mo@gmail.com>
2018-05-04 16:48:18 +0200

    start btn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './btn-3d.css';
 import WebcamCapture from './WebcamCapture';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      start: false
+    }
+  }
+
+  startCamera() {
+    this.setState({ start: true })
+  }
+
   render() {
     return (
       <div className="App">
-        <WebcamCapture />
+        {!this.state.start && <button onClick={() => this.startCamera()} className="btn-3d cyan">开始</button>}
+        {this.state.start && <WebcamCapture />}
       </div>
     );
   }
